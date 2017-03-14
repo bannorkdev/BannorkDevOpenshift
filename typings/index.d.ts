7faf9fb4e9a56f75363afeaeffb289acce98b421
bannorkdev <bannorkdev@gmail.com>
2017-03-14 20:46:00 +0700

    add file to visual studio

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/chai-http/index.d.ts" />
+/// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/object-assign/index.d.ts" />
